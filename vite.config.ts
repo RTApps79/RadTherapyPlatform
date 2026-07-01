6f9eae01ce65204cdde019ba726b19b45a0ffb05
Kevin Kindle <radtherapyapps@gmail.com>
2026-06-30 23:27:29 -0400

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  // This tells Vite: "When I deploy, my code is sitting in a subfolder called /RadTherapyPlatform/"
-  base: '/RadTherapyPlatform/',
-});
